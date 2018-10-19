90a46e908e83fadf74af4b288dca52150ed8e792
Luan <luann.porfirio@gmail.com>
2018-10-19 08:35:17 -0300

    funcao lat long atualizadas. novos atributos

--- request.py
+++ request.py
@@ -5,13 +5,16 @@ from os.path import isfile,join,abspath
 
 class Request:
     def __init__(self,rn,company,log,device,app,lat,lon,keyPerson,attr,
-        imgType,trueImage,truePC,img,pc,free):
+        imgType,trueImage,truePC,img,pc,imgName):
 
         self.requestNumber = rn
         self.companyCode = company
         self.logCode = log
         self.captureDeviceCode = device
-        self.appCode = app #tipo de solicitacao
+
+        #tipo de solicitacao
+        self.appCode = app 
+
         self.latitude = lat
         self.longitude = lon
         self.keyPerson = keyPerson
@@ -21,22 +24,29 @@ class Request:
         self.truePictureTree = truePC
         self.imageValidate = img
         self.validatePictureTree = pc
-        self.free = free
 
-        #self.imageName = imgName
+        self.imageName = imgName
+
         #self.companyCode,self.keyPerson,self.seqPerson = self.imageName.split('-')
         #self.seqPerson = self.seqPerson.split('.')[0]
 
-   def isLat(self.lat):
-       try:
-           if self.lat < 90 and self.lat> -90:
-               return 1
-       except:
-        return 0
+        self.gender, self.birth, self.imgDate = self.imageName.split('-')[-3:]
+        self.imgDate = self.imgDate.split('.')[0]
 
-   def isLon(self.lon):
+        self.idPerson = None
+
+    #def getData(imageName):
+
+    def isLatitude(self):
         try:
-            if self.lon < 180 and self.lon > -180:
+            if self.latitude < 90 and self.latitude> -90:
+                return 1
+        except:
+            return 0
+
+    def isLongitude(self):
+        try:
+            if self.longitude < 180 and self.longitude > -180:
                 return 1
         except:
             return 0 
