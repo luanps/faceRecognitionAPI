336fe212e0134af325029cc5e33ac92a8735c47d
Luan <luann.porfirio@gmail.com>
2018-10-19 08:32:36 -0300

    classe parametros de retorno

---
+++ genSelect.py
@@ -0,0 +1,7 @@
+for i in range(1,129):
+    if i%4:
+        d=' + '
+    else:
+        d=' + \n'
+    print('POW(tpl{:03}-%s,2)'.format(i),end=d)
+
---
+++ returnFile.py
@@ -0,0 +1,7 @@
+class Return:
+    def __init__(self,rn,status,idPerson):
+        self.requestNumber = rn
+        self.status = status
+        self.idPerson = idPerson
+
+
